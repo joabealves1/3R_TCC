7cfef92ad1f1d847488c7288dbf20842305d223d
Andre Felipe <andrefelipehb@hotmail.com>
2021-03-24 20:44:27 -0300

    Altreação besta

--- app/src/main/java/com/example/a3r/tela_login.java
+++ app/src/main/java/com/example/a3r/tela_login.java
@@ -11,7 +11,7 @@ public class tela_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
-
+//TODO alteração
 
     }
 }
