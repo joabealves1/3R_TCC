cedfcae878eedbf1770176970f4364bc179e6994
joabe <joabealves.1@outlook.com>
2021-03-24 20:13:36 -0300

    primeiro commit

--- app/src/main/java/com/example/a3r/tela_login.java
+++ app/src/main/java/com/example/a3r/tela_login.java
@@ -1,6 +1,17 @@
 package com.example.a3r;
 
-public class tela_login {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
+public class tela_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+
+
+    }
 }
